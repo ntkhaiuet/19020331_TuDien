ff6a9cb4bef25d61cdb8cee09e94f37d1b7ced4a
ntkhaiuet <ntkhaiuet@gmail.com>
2020-10-12 15:16:51 +0700

    command line cai tien lan 2

--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -18,16 +19,16 @@ public class DictionaryManagement {
             Word e = new Word(target.toLowerCase(), explain.toLowerCase());
             dic.arr.add(e);
         }
-        scan.close();
         return dic;
     }
 
     public Dictionary insertFromFile(Dictionary dic) throws FileNotFoundException {
-        Scanner scan = new Scanner(new File("dictionaries.txt"));
+        Scanner scan = new Scanner(new File("dictionaries.txt")).useDelimiter("\\s*:\\s*");
         while (scan.hasNext()) {
-            String line = scan.nextLine();
-            String[] arr = line.split("[   ]+",2);
-            Word e = new Word(arr[0].toLowerCase(), arr[1].toLowerCase());
+            String target = scan.next();
+            String explain = scan.nextLine();
+            explain = explain.substring(3);
+            Word e = new Word(target.toLowerCase(), explain.toLowerCase());
             dic.arr.add(e);
         }
         Collections.sort(dic.arr, new Word());
@@ -38,7 +39,6 @@ public class DictionaryManagement {
         Scanner scan = new Scanner(System.in);
         System.out.println("Nhap tu tieng Anh:");
         String en = scan.nextLine();
-        scan.close();
         int low = 0;
         int high = array.size();
         int mid = (low + high) / 2;
@@ -52,8 +52,73 @@ public class DictionaryManagement {
             }
         }
         if(!en.equalsIgnoreCase(array.get(mid).getWord_target()))
-            System.out.println("Khong tim thay tu vua nhap");
+            System.out.println("Khong tim thay tu vua nhap.");
         else
             System.out.println(array.get(mid).getWord_explain());
+    }
+
+    public void deleteWord (Dictionary dic){
+        try{
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Nhap tu can xoa bang tieng Anh:");
+            String word = scan.nextLine();
+            for (Word i : dic.arr){
+                if (i.getWord_target().equals(word)) {
+                    dic.arr.remove(dic.arr.indexOf(i));
+                }
+            }
+        }
+        catch (Exception ex){
+        }
+    }
+
+    public void fixWord (Dictionary dic) {
+        try{
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Nhap tu can sua bang tieng Anh:");
+            String en = scan.nextLine();
+            int low = 0;
+            int high = dic.arr.size();
+            int mid = (low + high) / 2;
+            while ((mid != low && mid != high)) {
+                if (dic.arr.get(mid).getWord_target().compareTo(en) > 0){
+                    high = mid;
+                    mid = (low + high) / 2;
+                }
+                else {
+                    low = mid;
+                    mid = (low + high) / 2;
+                }
+            }
+            if( !en.equalsIgnoreCase(dic.arr.get(mid).getWord_target()))
+                System.out.println("Khong tim thay tu vua nhap.");
+            else {
+                System.out.println(dic.arr.get(mid).getWord_target()+" : "+dic.arr.get(mid).getWord_explain());
+                System.out.println("Sua tu tieng Anh:" );
+                String fixEn = scan.nextLine();
+                System.out.println("Sua tu tieng Viet:" );
+                String fixVi = scan.nextLine();
+                Word fixWord = new Word(fixEn,fixVi);
+                dic.arr.set(mid,fixWord);
+            }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void dictionaryExportToFile(Dictionary dic) {
+        try {
+            File file = new File("dictionaries.txt");
+            FileWriter fileWrite = new FileWriter(file);
+
+            for(int i = 0; i < dic.arr.size(); i++) {
+                fileWrite.write(dic.arr.get(i).getWord_target()+ " : " + dic.arr.get(i).getWord_explain() + "\n");
+            }
+            fileWrite.close();
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
